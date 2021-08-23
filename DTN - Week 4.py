ff45710656c4ea991be8a103e93240ddffbc80c6
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-08-23 13:49:18 +1000

    Add other files to Sandbox

---
+++ DTN - Week 4.py
@@ -0,0 +1,18 @@
+# """
+# DTN - count letters in name
+# """
+#
+# VOWELS = "aeiouAEIOU"
+#
+# number_of_vowels = 0
+# name = str(input("Name: "))
+# for character in name:
+#     print(character)
+#     if character in VOWELS:
+#         number_of_vowels += 1
+# print("Out of {} letters, {} has {} vowels".format(len(name), name, number_of_vowels))
+
+# names = ['Bob', 'jimbo', 'saLLy']
+# for i in range(len(names)):
+#     names[i] = names[i].title()
+# print(names)
