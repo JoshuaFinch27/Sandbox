bcceaae0d3eb1b54193a73c1559124544cb12e58
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-08-30 13:28:43 +1000

    Add complete version of DTN - Week 6.py

--- DTN - Week 6.py
+++ DTN - Week 6.py
@@ -3,11 +3,10 @@ Lecture 6 DTN
 
 Write a list comprehension to produce a new list containing only the products that are on sale (True means its on sale)
 
-products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True],...]
-
-
-on_sale_products = []
-print(on_sale_products)
-
 """
 
+products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True], ["GitHub Swag", 0, False]]
+
+on_sale_products = [product for product in products if product[2]]
+print(products)
+print(on_sale_products)
