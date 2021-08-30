d87e1e2f7187d15608bb697e6bcb1e62f5006d94
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-08-30 13:19:03 +1000

    Add better formatting for list_comprehensions.py program

---
+++ DTN - Week 6.py
@@ -0,0 +1,13 @@
+"""
+Lecture 6 DTN
+
+Write a list comprehension to produce a new list containing only the products that are on sale (True means its on sale)
+
+products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True],...]
+
+
+on_sale_products = []
+print(on_sale_products)
+
+"""
+
