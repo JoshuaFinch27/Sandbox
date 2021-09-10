8c3c571ca4b2dfea26af5995291df604e056b595
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-09-10 12:17:01 +1000

    Add mess of stuff that was typed during a lecture, mess that doesn't work or serve much purpose.

--- product_classes.py
+++ product_classes.py
@@ -6,19 +6,40 @@
 #     pass
 
 class Product:
-    def __init__(self):
-        # print("Hello")
-        self.name = ""
-        self.price = 0.0
-        # self.is_on_sale = False  # Instance variable, persists outside(?) program
+    """Class docstring"""
+
+    default_rate = 0.2 # class variable (shared)
+
+    def __init__(self, name="", price=0.0, is_on_sale=False):
+        self.name = name
+        self.price = price
         self.is_on_sale = is_on_sale
+        self.sku = ""
 
     def __str__(self):
-        return f"{self.name} costs ${self.price} ({self.is_on_sale})"
+        on_sale_message = f" (on sale!)" if {self.is_on_sale} else ""
+        return f"{self.name} (SKU: {self.sku}) costs ${self.price:.2f} ({on_sale_message} ({self.default_rate})"
+
+    def __repr__(self):
+        return str(self)
+
+    def put_on_sale(self, discount_rate):
+        """Method docstring"""
+        self.price *= (1 - discount_rate)
+        self.is_on_sale = True
+
+    @staticmethod
+    def cheer():
+        print("Yay!")
+
+    def give(self, other, amount):
+        if self.price - amount < 0:
+            return False
+
+        self.price -+ amount
+        self.put_on_sale += amount
 
 
-    def __len__(self):
-        return 7
 
 # p = Product()
 # print(p)
@@ -28,29 +49,23 @@ class Product:
 # print(len(p))
 
 p = Product("Phone", 340.0, False)
+print(p)
+p.put_on_sale()
+print(p)
 
+p2 = Product("Horse", 500, True)
+print(p2)
+p2.put_on_sale()
+print(p2)
 
+print(p.defualt_rate, p2.default_rate)
+Product.default_rate = 0.9
+print(p.defualt_rate, p2.default_rate)
 
 
 
 products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True], ["GitHub Swag", 0, False]]
 
-on_sale_products = [product for product in products if product.is_on_sale]
-print(products)
+# on_sale_products = [product for product in products if product.is_on_sale]
+# print(products)
 # print(on_sale_products)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
