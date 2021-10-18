af02afcfd5eeb0092931a4ecc39bb015916ea3b0
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-10-18 15:21:32 +1000

    Add DTN's for Week 10 and 12 (no classes in Week 11)

---
+++ DTN - Weekly Programs/DTN - Week 10.py
@@ -0,0 +1,13 @@
+"""
+Write a function definition to take in a filename and return the longest line of that file.
+It should return a tuple of both (line numbers, length in characters)
+
+Write a function call to show how this would be used
+
+def find_longest_line():
+    open file
+    longest_line = 0
+    for line in
+
+
+"""
