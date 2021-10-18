c9f5a6551473b6ea8386284f674eb42c36294545
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-10-18 15:17:57 +1000

    Add DTN's for Week 10 and 12 (no classes in Week 11)

---
+++ DTN - Weekly Programs/DTN - Week 12.py
@@ -0,0 +1,6 @@
+"""
+Write a program to determine which is the largest file in the current directory, as defined by total number of
+characters in the file. You will need to use the OS module.
+
+
+"""
