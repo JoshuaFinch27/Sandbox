dc96f046112ec5f039736da3d28777425c59520c
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2021-10-19 09:31:43 +1000

    Add updated DTN - Week 12.py

--- DTN - Weekly Programs/DTN - Week 12.py
+++ DTN - Weekly Programs/DTN - Week 12.py
@@ -2,5 +2,19 @@
 Write a program to determine which is the largest file in the current directory, as defined by total number of
 characters in the file. You will need to use the OS module.
 
+Write the 2 tests (first!) using assert and the a function that returns whether its input parameter is odd or not.
+
 
 """
+
+
+# 1
+
+# 2
+
+def is_odd(number):
+    return number
+
+
+assert is_odd(1)
+assert not is_odd(2)
